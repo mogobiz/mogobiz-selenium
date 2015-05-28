954878a0d7cd91c6712c3d02223644ecd5c036c4
Samy Dindane <samy@dindane.com>
2015-05-28 11:10:24 +0200

    Add the code

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "mogobiz-selenium"
+
+Revolver.settings
+
+libraryDependencies in ThisBuild ++= Seq(
+  "org.codehaus.groovy" % "groovy-all" % "2.3.5"
+)
+
+mainClass in Revolver.reStart := Some("com.mogobiz.selenium.run.RestSelenium")
---
+++ src/main/scala/com/mogobiz/selenium/run/RestSelenium.scala
@@ -0,0 +1,49 @@
+package com.mogobiz.selenium.run
+
+import akka.actor.Props
+import akka.io.IO
+import com.mogobiz.pay.config.{MogopayRoutes}
+import com.mogobiz.run.actors.{ActorSystemLocator}
+import com.mogobiz.run.config
+import com.mogobiz.run.es.EmbeddedElasticSearchNode
+import com.mogobiz.run.jobs.CleanCartJob
+import com.mogobiz.run.mail.EmailService
+import com.mogobiz.run.config.MogobizRoutes
+import com.mogobiz.system.{RoutedHttpService, BootedMogobizSystem}
+import spray.can.Http
+import com.mogobiz.launch.run.Settings
+
+/**
+ * Created by yoannbaudy on 19/12/2014.
+ */
+object RestSelenium extends App with BootedMogobizSystem with MogobizRoutes  with MogopayRoutes with EmbeddedElasticSearchNode {
+
+  ActorSystemLocator(system)
+
+  val p = config.Settings.config.getString("selenium-path-data")
+  val esNode = startES(p)
+  sys.addShutdownHook({stopES(esNode)})
+
+  //init the email service with the system Actor
+  EmailService(system, "emailService")
+
+  com.mogobiz.pay.jobs.ImportRatesJob.start(system)
+  com.mogobiz.pay.jobs.ImportCountriesJob.start(system)
+  com.mogobiz.pay.jobs.CleanAccountsJob.start(system)
+
+  override val bootstrap = {
+    super[MogopayRoutes].bootstrap()
+    com.mogobiz.session.boot.DBInitializer()
+    com.mogobiz.notify.boot.DBInitializer()
+  }
+
+  //init jobs
+//  CleanCartJob.start(system)
+
+  override val routes = super[MogobizRoutes].routes ~ super[MogopayRoutes].routes
+
+  override val routesServices = system.actorOf(Props(new RoutedHttpService(routes)))
+
+  IO(Http)(system) ! Http.Bind(routesServices, interface = Settings.Interface, port = Settings.Port)
+
+}
