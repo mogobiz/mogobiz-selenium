ef22f33751a7931fa26786fe447b02e6705a5338
hayssams <hayssam.saleh@ebiznext.com>
2016-06-26 17:25:01 +0200

    new scala fmt plugin with vertical alignment

--- src/main/scala/com/mogobiz/selenium/run/RestSelenium.scala
+++ src/main/scala/com/mogobiz/selenium/run/RestSelenium.scala
@@ -3,22 +3,27 @@ package com.mogobiz.selenium.run
 import akka.actor.Props
 import akka.io.IO
 import spray.can.Http
-import com.mogobiz.pay.config.{ MogopayRoutes }
+import com.mogobiz.pay.config.{MogopayRoutes}
 import com.mogobiz.run.config
 import com.mogobiz.run.es.EmbeddedElasticSearchNode
 import com.mogobiz.run.jobs.CleanCartJob
 import com.mogobiz.run.config.MogobizRoutes
-import com.mogobiz.system.{ ActorSystemLocator, RoutedHttpService, BootedMogobizSystem }
+import com.mogobiz.system.{ActorSystemLocator, RoutedHttpService, BootedMogobizSystem}
 import com.mogobiz.launch.run.Settings
 
 /**
- * Created by yoannbaudy on 19/12/2014.
- */
-object RestSelenium extends App with BootedMogobizSystem with MogobizRoutes with MogopayRoutes with EmbeddedElasticSearchNode {
+  * Created by yoannbaudy on 19/12/2014.
+  */
+object RestSelenium
+    extends App
+    with BootedMogobizSystem
+    with MogobizRoutes
+    with MogopayRoutes
+    with EmbeddedElasticSearchNode {
 
   ActorSystemLocator(system)
 
-  val p = config.Settings.config.getString("selenium-path-data")
+  val p      = config.Settings.config.getString("selenium-path-data")
   val esNode = startES(p)
   sys.addShutdownHook({ stopES(esNode) })
 
